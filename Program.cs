396932574711e4aaad5738d188da12cc6b35080e
alehapit <alehapit@gmail.com>
2026-06-11 13:27:29 +0700

    nha_dev

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@
 Console.WriteLine("Hello, World!");
 PhanSo a = new PhanSo(2, 4);
 PhanSo b = new PhanSo(3, 4);
+PhanSo c = new PhanSo(1, 2);
 
 Console.WriteLine($"a = {a}");
 Console.WriteLine($"b = {b}");
-
+Console.WriteLine($"c = {c}");
 
 Console.WriteLine($"a + b = {a + b}");
 Console.WriteLine($"a - b = {a - b}");
